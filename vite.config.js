d699d55e513c80fcb7c2a0d6c7991b3abcf79e00
anacastrosanches-dotcom <ana.castrosanches@gmail.com>
2026-05-07 22:08:48 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: `assets/app.js`,
-        chunkFileNames: `assets/[name].js`,
-        assetFileNames: `assets/[name].[ext]`,
-      },
-    },
-  },
 });
